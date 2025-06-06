eee6b9a206f69d4649443715b54950136c0a0401
Isolate <50405art@naver.com>
2025-06-06 15:41:01 +0900

    페이지 디자인 수정중

--- shop/src/main/java/com/example/shop/Member.java
+++ shop/src/main/java/com/example/shop/Member.java
@@ -11,7 +11,7 @@ import lombok.ToString;
 @ToString
 public class Member {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="member_id")
+//    @Column(name="member_id")
     private Integer id;
     private String username;
     private String password;
